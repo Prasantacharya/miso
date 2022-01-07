9f601a78208027daf9d78316300c0beaf036e5fb
Prasantacharya <prasantacharyabusiness@gmail.com>
2022-01-07 15:23:07 -0500

    refactor and rewrite in python, begins here

--- interface/main.js
+++
@@ -1,9 +0,0 @@
-// handle command line stuff here
-
-// includes
-
-function main(){
-
-}
-
-
--- shared/dl-options.js
+++
@@ -1,8 +0,0 @@
-/* TODO: add different download options like
- 1) .cbz
- 2) pdf
- 3) png
- 4)
-*/
-const common = require("../shared/shared.js");
-const archiver = require('archiver');
